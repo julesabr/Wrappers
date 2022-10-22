91826087dd4fc0ae46a0810126e60aae75ada41e
Julian Sangillo <juliansangillo@gmail.com>
2022-10-22 14:15:13 -0400

    docs: Updated discordfx template to 1.1

--- docs/templates/discordfx/styles/toggle-theme.js
+++ docs/templates/discordfx/styles/toggle-theme.js
@@ -1,48 +1,50 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-const darkTheme = "colors-dark.css";
-const lightTheme = "colors-light.css";
-const darkCodeTheme = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.1/styles/night-owl.min.css";
-const lightCodeTheme = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/styles/base16/github.min.css";
-const sw = document.getElementById('switch-style');
 
-const theme = document.getElementById('theme');
-const href = theme.getAttribute('href');
-const path = href.split('/');
+const darkColors = "colors-dark.css";
+const lightColors = "colors-light.css";
+const darkCode = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.1/styles/night-owl.min.css";
+const lightCode = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/styles/base16/github.min.css";
 
-const codeTheme = document.getElementById('code-theme');
+let sw = document.getElementById('switch-dark');
 
-function setTheme(currentTheme) {
-    theme.setAttribute('href', path.slice(0, path.length - 1).concat(currentTheme).join('/'));
+function setColors(currentColors) {
+    const path = colorsTheme.getAttribute('href').split('/');
+    colorsTheme.setAttribute('href', path.slice(0, path.length - 1).concat(currentColors).join('/'));
 }
 
-function setCodeTheme(currentCodeTheme) {
-    codeTheme.setAttribute('href', currentCodeTheme);
+function setCode(currentCode) {
+    codeTheme.setAttribute('href', currentCode);
 }
 
-function toggleTheme() {
-    let currentTheme = darkTheme;
-    let currentCodeTheme = darkCodeTheme;
-    if (!this.checked) {
-        currentTheme = lightTheme;
-        currentCodeTheme = lightCodeTheme;
+function toggleTheme(dark) {
+    let currentColors = darkColors;
+    let currentCode = darkCode;
+    if (!dark) {
+        currentColors = lightColors;
+        currentCode = lightCode;
     }
 
-    setTheme(currentTheme);
-    setCodeTheme(currentCodeTheme);
-
-    if (window.localStorage) {
-        localStorage.setItem("theme", currentTheme);
-        localStorage.setItem("code-theme", currentCodeTheme);
-    }
+    setColors(currentColors);
+    setCode(currentCode);
 }
 
-if (sw)
-    sw.addEventListener("change", toggleTheme);
+function onChange() {
+    toggleTheme(this.checked);
+    if (this.checked)
+        localStorage.setItem("theme", "dark");
+    else 
+        localStorage.setItem("theme", "light");
+}
 
-if (window.localStorage && window.localStorage.getItem("theme") && !href.includes(localStorage.getItem("theme"))) {
-    setTheme(localStorage.getItem("theme"));
-    setCodeTheme(localStorage.getItem("code-theme"));
+if (window.localStorage) {
+    if(localStorage.getItem("theme") === "light") {
+       sw.remove();
+       sw = document.getElementById('switch-light');
+    }
+    else
+       document.getElementById('switch-light').remove();
 
-    if (localStorage.getItem("theme") === lightTheme)
-        sw.checked = false;
+    sw.classList.remove("hidden");
+
+    document.getElementById('theme').addEventListener("change", onChange);
 }
